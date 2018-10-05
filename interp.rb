73195211fd5bcb3d6c71cc3c0f643c0bd18b0634
Sankichi <sankichi92@sankichi.net>
2018-10-05 13:28:03 +0900

    6-1

--- interp.rb
+++ interp.rb
@@ -163,10 +163,12 @@ def evaluate(exp, env)
 
   # You don't need advices anymore, do you?
   when "ary_new"
-    raise(NotImplementedError) # Problem 6
+    exp[1..-1].map do |e|
+      evaluate(e, env)
+    end
 
   when "ary_ref"
-    raise(NotImplementedError) # Problem 6
+    evaluate(exp[1], env)[evaluate(exp[2], env)]
 
   when "ary_assign"
     raise(NotImplementedError) # Problem 6
